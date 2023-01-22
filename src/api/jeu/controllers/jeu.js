fdd1925979b357a8e6ff13882d6eeed7557f93cc
kipik <kipik.kipik@gmail.com>
2023-01-23 00:54:17 +0100

    base v0.8

--- src/api/jeu/controllers/jeu.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * jeu controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::jeu.jeu');
--- src/api/jeu/routes/jeu.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * jeu router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::jeu.jeu');
