b7139617226d56466f431b545e9cf7bfce658783
kipik <kipik.kipik@gmail.com>
2023-02-01 02:28:27 +0100

    Install fonts (Merriweather + Merriweather Sans

--- cwowd/frontend/app.vue
+++ cwowd/frontend/app.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
     <h1>C'est ça le nouveau Cwowd ? Ca a l'air tout pourri</h1> 
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex iure numquam obcaecati totam, ab dignissimos natus quia, eos, unde earum illo sit commodi nemo assumenda atque dolorem repellat ducimus eius.</p>
   </div>
 </template>
+
+<style>
+html {
+  font-family: 'Merriweather', serif;
+  font-weight: 300;
+  font-size: 1.1rem;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  font-family: 'Merriweather Sans', sans-serif;
+}
+</style>
