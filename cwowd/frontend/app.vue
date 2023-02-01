083966397ea8b90c3b2fa640fad12e6e594c7915
kipik <kipik.kipik@gmail.com>
2023-02-01 01:05:59 +0100

    Install Nuxt

---
+++ cwowd/frontend/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>C'est ça le nouveau Cwowd ? Ca a l'air tout pourri</h1> 
+  </div>
+</template>
---
+++ cwowd/frontend/nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    modules: ['@nuxtjs/strapi'],
+    strapi: {
+      url: 'http://localhost:1338'
+    },
+})
+
